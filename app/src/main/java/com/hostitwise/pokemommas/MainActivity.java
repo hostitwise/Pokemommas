a6692ce4629308d48927ce5472175d5cb04b10ce
Drake Eidukas <eidukas2@illinois.edu>
2016-08-09 12:08:16 -0500

    Initial Setup with buttons, more activities

--- app/src/main/java/com/hostitwise/pokemommas/MainActivity.java
+++ app/src/main/java/com/hostitwise/pokemommas/MainActivity.java
@@ -6,6 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+/**
+ * There will be a stored preference file (internally just an xml file) that stores user preferences for this activity.
+ * Once the user logs into either child or parent, it should have a boolean indicating if they are a child or a parent
+ * This persists until the user hits logout.
+ * Based on this stored boolean, it will either open directly to the child intent, parent intent, or this launch screen.
+ */
 public class MainActivity extends AppCompatActivity {
 
     public static final String PREFS_NAME="UserPreferenceFile";
@@ -13,8 +19,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        SharedPreferences settings = getSharedPreferences(PREFS_NAME,0);
+        SharedPreferences settings = getPreferences(MODE_PRIVATE);
         setContentView(R.layout.activity_main);
+
+    }
+
+    private boolean preferenceExists(String preferenceFlag){
+        SharedPreferences settings = getPreferences(MODE_PRIVATE);
+        if(settings.getString(preferenceFlag,null)==null){
+            return false;
+        }
+        return true;
+    }
+    //return null if preference doesn't exist.
+    private String getPreferenceString(String preferenceFlag){
+        if(!preferenceExists(preferenceFlag)){
+            return null;
+        }
+        else{
+            return getPreferences(MODE_PRIVATE).getString((preferenceFlag),null);
+        }
     }
 
     public void launchChildStart(View view){
