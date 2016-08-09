473f91a8853f7d1d51e2c94e675d1135df931826
Drake Eidukas <eidukas2@illinois.edu>
2016-08-09 13:01:01 -0500

    Initial Setup, firebase setup started.

---
+++ app/src/main/java/com/hostitwise/pokemommas/Constants.java
@@ -0,0 +1,8 @@
+package com.hostitwise.pokemommas;
+
+/**
+ * Created by Drake on 8/9/2016.
+ */
+public class Constants {
+    public static final String FIREBASE_URL="https://pokemommas-428dd.firebaseio.com";
+}
