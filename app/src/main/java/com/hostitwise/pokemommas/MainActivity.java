0e065e28c2e8f08b22ce2cd3c1b0c25a7f9d7f60
Drake Eidukas <eidukas2@illinois.edu>
2016-08-09 11:01:42 -0500

    Initial Setup with buttons

--- app/src/main/java/com/hostitwise/pokemommas/MainActivity.java
+++ app/src/main/java/com/hostitwise/pokemommas/MainActivity.java
@@ -2,6 +2,7 @@ package com.hostitwise.pokemommas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchChildStart(View view){
+
+    }
+    public void launchParentStart(View view){
+
     }
 }
