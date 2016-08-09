c57026ea689ea67d18c8e02ee38a3a88ba5d8589
Drake Eidukas <eidukas2@illinois.edu>
2016-08-09 11:29:31 -0500

    Initial Setup with buttons, more activities

--- app/src/main/java/com/hostitwise/pokemommas/MainActivity.java
+++ app/src/main/java/com/hostitwise/pokemommas/MainActivity.java
@@ -1,21 +1,28 @@
 package com.hostitwise.pokemommas;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String PREFS_NAME="UserPreferenceFile";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME,0);
         setContentView(R.layout.activity_main);
     }
 
     public void launchChildStart(View view){
-
+        Intent intent = new Intent(this, ChildActivity.class);
+        startActivity(intent);
     }
     public void launchParentStart(View view){
-
+        Intent intent = new Intent(this, ParentActivity.class);
+        startActivity(intent);
     }
 }
