24073810d4cd4e6278925cd85095a4d3b3e2830c
Drake Eidukas <eidukas2@illinois.edu>
2016-08-09 00:21:27 -0500

    Initial Setup

---
+++ app/src/main/java/com/hostitwise/pokemommas/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hostitwise.pokemommas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
